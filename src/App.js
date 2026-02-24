e393ee6ed3448fe9eb3d7e6305634bc0e3203673
William Sephton <willsephton1234@gmail.com>
2026-02-24 17:17:07 +0000

    privacy policy for apps 3

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Portfolio from "./components/Portfolio";
 import Header from "./components/Header";
 import Expertise from "./components/Expertise";
